f9e287f2de2b9df26dceda13a3bf8aa78b4ffb7d
Yeison Ascanio <ydascanio@comfenalcosantander.com.co>
2024-12-03 17:46:05 -0500

    Ajustamos el primer prompt

--- src/services/bot/service.ts
+++ src/services/bot/service.ts
@@ -6,25 +6,25 @@ const msgStartChat =
 el inventario de camaras de seguridad que tenemos:\
 Cámara de seguridad Wi-Fi / Ethernet 3 Mpx robotizada con seguidor de movimiento para exterior, precio: $299.900 COP\
 Cámara de seguridad Wi-Fi Full HD robotizada con seguidor de movimiento, percio: $179.900 COP	\
-Cámara de seguridad Wi-Fi* 3 Mpx robotizada con seguidor de movimiento para socket, percio:	199.900	COP\
-Cámara de seguridad Wi-Fi Full HD fija, percio:	139.900 COP\
-Cámara de seguridad Wi-Fi* / Ethernet 3 MPx robotizada con seguidor de movimiento, percio:	219.900 COP\
-Cámara de seguridad Wi-Fi 3 Mpx fija para exterior tipo bala, percio:	199.900 COP\
-Cámara de seguridad Wi-Fi Full HD fija con batería recargable, percio:	379.900 COP	\
-Sistema de seguridad CCTV con DVR pentahíbrido de 12 canales, 4 cámaras, disco duro y monitoreo por Internet, percio:	1.679.900 COP	\
-Sistema de seguridad CCTV con DVR pentahíbrido de 6 canales, 4 cámaras, disco duro y monitoreo por Internet, percio:	1.519.900 COP\
-Cámara de seguridad Wi-Fi* / Ethernet Full HD fija con reflector LED, compatible con asistentes de voz, percio:	189.900 COP\
-Cámara de seguridad Wi-Fi* Full HD con reflector LED fija, percio:	439.900 COP\
-Soporte para cámaras CCTV con brazo flexible y tornillo universal, percio:	41.900 COP\
-Cámara de seguridad CCTV digital Full HD, tipo domo, tetrahíbrida, percio:	119.900 COP\
-Cámara de seguridad CCTV digital Full HD, para exterior, tipo mini bala, tetrahíbrida, percio:, percio:	119.900 COP\
-Cámara de seguridad CCTV digital Full HD, tipo mini bala, tetrahíbrida, metálica, percio:	139.900 COP\
-Cámara de seguridad CCTV simulada (dummy) tipo bala, percio:	34.900 COP	\
-Cámara de seguridad Wi-Fi* Full HD 360°, percio:	199.900 COP\
-Cámara de seguridad Wi-Fi* / Ethernet Full HD fija para exterior tipo mini bala, percio:	289.900 COP\
-Cámara de seguridad Wi-Fi HD fija con visión 360°, percio:	114.900 COP	\
-Reloj despertador con cámara oculta, percio:	94.900 COP\
-Cámara de seguridad CCTV simulada (dummy) tipo domo, percio:	31.900 COP\
+Cámara de seguridad Wi-Fi* 3 Mpx robotizada con seguidor de movimiento para socket, percio:	$199.900	COP\
+Cámara de seguridad Wi-Fi Full HD fija, percio:	$139.900 COP\
+Cámara de seguridad Wi-Fi* / Ethernet 3 MPx robotizada con seguidor de movimiento, percio:	$219.900 COP\
+Cámara de seguridad Wi-Fi 3 Mpx fija para exterior tipo bala, percio:	$199.900 COP\
+Cámara de seguridad Wi-Fi Full HD fija con batería recargable, percio:	$379.900 COP	\
+Sistema de seguridad CCTV con DVR pentahíbrido de 12 canales, 4 cámaras, disco duro y monitoreo por Internet, percio:	$1.679.900 COP	\
+Sistema de seguridad CCTV con DVR pentahíbrido de 6 canales, 4 cámaras, disco duro y monitoreo por Internet, percio:	$1.519.900 COP\
+Cámara de seguridad Wi-Fi* / Ethernet Full HD fija con reflector LED, compatible con asistentes de voz, percio:	$189.900 COP\
+Cámara de seguridad Wi-Fi* Full HD con reflector LED fija, percio:	$439.900 COP\
+Soporte para cámaras CCTV con brazo flexible y tornillo universal, percio:	$41.900 COP\
+Cámara de seguridad CCTV digital Full HD, tipo domo, tetrahíbrida, percio:	$119.900 COP\
+Cámara de seguridad CCTV digital Full HD, para exterior, tipo mini bala, tetrahíbrida, percio:, percio:	$119.900 COP\
+Cámara de seguridad CCTV digital Full HD, tipo mini bala, tetrahíbrida, metálica, percio:	$139.900 COP\
+Cámara de seguridad CCTV simulada (dummy) tipo bala, percio:	$34.900 COP	\
+Cámara de seguridad Wi-Fi* Full HD 360°, percio:	$199.900 COP\
+Cámara de seguridad Wi-Fi* / Ethernet Full HD fija para exterior tipo mini bala, percio:	$289.900 COP\
+Cámara de seguridad Wi-Fi HD fija con visión 360°, percio:	$114.900 COP	\
+Reloj despertador con cámara oculta, percio:	$94.900 COP\
+Cámara de seguridad CCTV simulada (dummy) tipo domo, percio:	$31.900 COP\
 solo limitate a este inventario ahora empeza con un saludo breve de forma divertida. Todos los prompts digitalos tene siempre en cuenta esta información";
 
 export const sendChat = async (msg: string | null): Promise<resBot> => {
